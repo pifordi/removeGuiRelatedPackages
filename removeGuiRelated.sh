71349516eabb0688a93c6141eb32bf4792aa00b1
Semih YILDIRIM <semih.yildirim@outlook.com>
2018-05-11 11:50:02 +0300

    Create removeGuiRelated.sh

---
+++ removeGuiRelated.sh
@@ -0,0 +1,12 @@
+sudo apt purge --auto-remove vlc*
+sudo apt purge --auto-remove sonic-pi*
+sudo apt purge --auto-remove firefox*
+sudo apt purge --auto-remove scratch*
+sudo apt purge --auto-remove welcome*
+sudo apt purge --auto-remove thunderbird*
+sudo apt purge --auto-remove pidgin*
+sudo apt purge --auto-remove libreoffice*
+
+and then execute;
+sudo apt clean
+sudo apt autoremove
