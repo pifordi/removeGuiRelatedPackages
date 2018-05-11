fa0c926e393eaf83ff2bc3276a13ae0f828c3da6
Semih YILDIRIM <semih.yildirim@outlook.com>
2018-05-11 11:52:30 +0300

    git not installed
    install git on your ubuntu

--- removeGuiRelated.sh
+++ removeGuiRelated.sh
@@ -1,3 +1,7 @@
+#Firstly, install git on your ubuntu by typing
+sudo apt install git
+
+#And install packages
 sudo apt purge --auto-remove vlc*
 sudo apt purge --auto-remove sonic-pi*
 sudo apt purge --auto-remove firefox*
@@ -7,6 +11,6 @@ sudo apt purge --auto-remove thunderbird*
 sudo apt purge --auto-remove pidgin*
 sudo apt purge --auto-remove libreoffice*
 
-and then execute;
+#And then execute;
 sudo apt clean
 sudo apt autoremove
