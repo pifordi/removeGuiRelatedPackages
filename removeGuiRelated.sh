d271a071edb7518304d6457b6cccde48308e17c0
Semih YILDIRIM <semih.yildirim@outlook.com>
2018-05-15 01:24:19 +0300

    Update removeGuiRelated.sh

--- removeGuiRelated.sh
+++ removeGuiRelated.sh
@@ -2,14 +2,14 @@
 sudo apt install git
 
 #And install packages
-sudo apt purge --auto-remove vlc*
-sudo apt purge --auto-remove sonic-pi*
-sudo apt purge --auto-remove firefox*
-sudo apt purge --auto-remove scratch*
-sudo apt purge --auto-remove welcome*
-sudo apt purge --auto-remove thunderbird*
-sudo apt purge --auto-remove pidgin*
-sudo apt purge --auto-remove libreoffice*
+sudo apt purge --auto-remove vlc* -y
+sudo apt purge --auto-remove sonic-pi* -y
+sudo apt purge --auto-remove firefox* -y
+sudo apt purge --auto-remove scratch* -y
+sudo apt purge --auto-remove welcome* -y
+sudo apt purge --auto-remove thunderbird* -y
+sudo apt purge --auto-remove pidgin* -y
+sudo apt purge --auto-remove libreoffice* -y
 
 #And then execute;
 sudo apt clean
